8c56fb842310aab30992cc2547da8ca3c65aacd6
benjaminaheath <64213183+benjaminaheath@users.noreply.github.com>
2023-03-01 13:23:33 +0000

    add test_blink

---
+++ apps/test_blink/test_blink.c
@@ -0,0 +1,16 @@
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+int main(){
+    if(!cyw43_arch_init()){
+        return -1;
+    }
+
+    while(true){
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,1);
+        sleep_ms(1000);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN,0);
+        sleep_ms(1000);
+    }
+    return 0;
+}
